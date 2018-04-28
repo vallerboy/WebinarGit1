7f5354f2123255005bb4d2eb27b22d9b5f48b9de
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-04-28 15:58:35 +0200

    Added comment to class

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -7,7 +7,7 @@ public class LambdaTest {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>();
         names.addAll(Arrays.asList("oskar", "damian", "Ada", "Kamila", "Tomek", "Dorota"));
-
+        ///asdsadad
         names.sort((s, s1) -> s1.compareTo(s));
     }
 }
